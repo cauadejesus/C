4ad9705c19cf33217267be30add4954daea46c76
Cauã de Jesus <cauadejesusaguiar0@gmail.com>
2026-03-21 12:02:03 -0300

    ex1 seq1

---
+++ 22602063_caua_aguiar_ex_1_1_seq_tb_.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <locale.h>
+int main() {
+    setlocale(LC_ALL, "Portuguese");
+
+    float a, b, c, media;
+
+    printf("Digite tres numeros: ");
+    scanf("%f %f %f", &a, &b, &c);
+
+    media = (a + b + c) / 3;
+
+    printf("Media = %.2f\n", media);
+
+    return 0;
+}
